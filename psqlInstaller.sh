d2f20b954a78651e4ef66d16e67141cec2619ff8
udit4 <udit.gera@tokopedia.com>
2022-05-06 15:04:26 +0530

    added note in scripts

--- psqlInstaller.sh
+++ psqlInstaller.sh
@@ -1,10 +1,11 @@
-# NOTE :: 
+# NOTE ::
 #
 # before running the file, just make sure to give provide it complete executable rights, by running this -
 # e.g chmod a+x psqlInstaller.sh
 #
 # also for using psql, once script is executed swith over to postgres account on the server using this -
 # sudo -i -u postgres
+# for running this script just do -> ./psqlInstaller.sh 
 
 
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
