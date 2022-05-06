eb08e3b808f5dd3da3608a2dd6308ff613007887
udit4 <udit.gera@tokopedia.com>
2022-05-06 14:53:35 +0530

    added script for installing mysql on instance

---
+++ mysqlInstaller.sh
@@ -0,0 +1,6 @@
+# before running the file, just make sure to give provide it complete executable rights, by running this -
+# e.g chmod a+x mySqlInstaller.sh 
+
+wget -c https://repo.mysql.com//mysql-apt-config_0.8.13-1_all.deb
+sudo dpkg -i mysql-apt-config_0.8.13-1_all.deb
+sudo apt-get install mysql-server
