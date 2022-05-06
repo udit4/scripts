dc8e4764f31252131084b55d61b1048bd8c436e3
udit4 <udit.gera@tokopedia.com>
2022-05-06 15:00:13 +0530

    added script for psql installer

--- mysqlInstaller.sh
+++ mysqlInstaller.sh
@@ -1,5 +1,7 @@
+# NOTE ::
+#
 # before running the file, just make sure to give provide it complete executable rights, by running this -
-# e.g chmod a+x mySqlInstaller.sh 
+# e.g chmod a+x mySqlInstaller.sh
 
 wget -c https://repo.mysql.com//mysql-apt-config_0.8.13-1_all.deb
 sudo dpkg -i mysql-apt-config_0.8.13-1_all.deb
---
+++ psqlInstaller.sh
@@ -0,0 +1,12 @@
+# NOTE :: 
+#
+# before running the file, just make sure to give provide it complete executable rights, by running this -
+# e.g chmod a+x psqlInstaller.sh
+#
+# also for using psql, once script is executed swith over to postgres account on the server using this -
+# sudo -i -u postgres
+
+
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get install postgresql
